8fabaf86b13ddae02892bb8b8206c7cb3e7d276a
flwlss <dimaandreev1337@gmail.com>
2021-08-29 19:17:04 +0700

    fix contacts, add pop-up window

--- script.js
+++ script.js
@@ -2,25 +2,104 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("about-each__slider");
+  var i;
+  var slides = document.getElementsByClassName("about-each__slider");
 
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
-    }
-    slides[slideIndex - 1].style.display = "block"
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex - 1].style.display = "block"
 }
+
+
+var arrowOne = document.querySelector(".arrow-one");
+var openOne = document.querySelector(".form-one");
+var closeOne = document.querySelector(".form-close-one");
+arrowOne.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openOne.classList.add("form");
+});
+closeOne.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openOne.classList.remove("form");
+});
+
+
+var arrowTwo = document.querySelector(".arrow-two");
+var openTwo = document.querySelector(".form-two");
+var closeTwo = document.querySelector(".form-close-two");
+arrowTwo.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openTwo.classList.add("form");
+});
+closeTwo.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openTwo.classList.remove("form");
+});
+
+
+var arrowThree = document.querySelector(".arrow-three");
+var openThree = document.querySelector(".form-three");
+var closeThree = document.querySelector(".form-close-three");
+arrowThree.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openThree.classList.add("form");
+});
+closeThree.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openThree.classList.remove("form");
+});
+
+
+var arrowFour = document.querySelector(".arrow-four");
+var openFour = document.querySelector(".form-four");
+var closeFour = document.querySelector(".form-close-four");
+arrowFour.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFour.classList.add("form");
+});
+closeFour.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFour.classList.remove("form");
+});
+
+
+var arrowFive = document.querySelector(".arrow-five");
+var openFive = document.querySelector(".form-five");
+var closeFive = document.querySelector(".form-close-five");
+arrowFive.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFive.classList.add("form");
+});
+closeFive.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openFive.classList.remove("form");
+});
+
+
+var arrowSix = document.querySelector(".arrow-six");
+var openSix = document.querySelector(".form-six");
+var closeSix = document.querySelector(".form-close-six");
+arrowSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.add("form");
+});
+closeSix.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openSix.classList.remove("form");
+});
+
